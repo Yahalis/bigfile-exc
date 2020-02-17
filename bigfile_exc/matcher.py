c086a8effe5b2c9e5aaccf3bde4d818df48ea022
Yahali Sherman <ysherman@heed-global.com>
2020-02-17 23:53:00 +0200

    -
    Signed-off-by: Yahali Sherman <ysherman@heed-global.com>

--- bigfile_exc/matcher.py
+++ bigfile_exc/matcher.py
@@ -7,15 +7,24 @@ import nltk
 from bigfile_exc import ChunkMatches
 from bigfile_exc import Aggregator
 
-
 class Matcher:
-    def __init__(self, aggregator: Aggregator, maxsize=50):
+    def __init__(self, aggregator: Aggregator, maxsize=50) -> None:
+        """
+        Initialize the queues and keep params
+        :param aggregator: Aggregator instance to use
+        :param maxsize: Maximul queue size, will force chunker to wait if full
+        """
         self.__aggregator = aggregator
         self.__matchQueue = queue.Queue(maxsize=maxsize)
         self.__sentinel = 'Done'
         self.__counterLock=threading.Lock()
 
-    def run(self, numWorkers=4) -> None:
+    def run(self, numWorkers:int) -> None:
+        """
+        Running the workers.
+        :param numWorkers: num workers to run.
+        :return:
+        """
         self.__numWorkers = numWorkers
         self.__doneWorkers=0
         self.__workers = [threading.Thread(
@@ -28,7 +37,11 @@ class Matcher:
 
 
     def worker(self, id: int) -> None:
-        # print('start matching worker #'+str(id+1))
+        """
+        worker is listening on the queue and doing the match of the given chunk chunk.
+        :param id: worker id (FFU)
+        :return:
+        """
         for aggData in iter(self.__matchQueue.get, self.__sentinel):
             kwargs = aggData
             self.do_match(**kwargs)
@@ -42,18 +55,26 @@ class Matcher:
             self.__aggregator.terminate()
 
     def terminate(self) -> None:
+        """
+        Put termination marker at the end of the Queue (per worker)
+        :return:
+        """
         for i in range(self.__numWorkers):
             self.__matchQueue.put(self.__sentinel, block=True)
 
     def match(self, chunk: str, names: set, offset: int, chunkId: int) -> None:
+        "Put match instructions in the queue"
+        # this will wait if the Queue is full
         self.__matchQueue.put({'chunk': chunk, 'names': names, 'offset': offset, 'chunkId': chunkId}, block=True)
 
     def do_match(self, chunk: str, names: set, offset: int, chunkId: int) -> None:
         """
-
-        :param chunk:
-        :param names:
-        :param offset:
+        Main matching function.
+        using nltk regexp_span_tokenize to get all word locations, based on the letters not constructing tokens as splitters.
+        keeping the newline legal in order to be able to easily find and count lines, thus special treatmet if newline is in the token.
+        :param chunk: the chunk to match
+        :param names: set of names
+        :param offset: offset of the chunk in the file.
         :return:
         """
         nLines = 1
@@ -77,7 +98,3 @@ class Matcher:
         chunkMatches.lineCount = nLines
         self.__aggregator.put_agg_data(chunkId, chunkMatches)
 
-
-if __name__ == '__main__':
-    def test():
-        pass
